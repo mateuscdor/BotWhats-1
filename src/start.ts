90758c8d0e6729a2156f462fa71721fa9060828d
EduardoPH <dudu19424irma@gmail.com>
2022-03-30 09:23:46 -0300

    okkk

--- src/start.ts
+++ src/start.ts
@@ -2,11 +2,4 @@ import bot from './bot'
 import cors from 'cors'
 import express from 'express'
 
-const app = express()
-app.use(cors())
-
 bot()
-
-app.listen(process.env.PORT, (r) =>
-  console.log(`API subiu na porta ${process.env.PORT}`)
-);
